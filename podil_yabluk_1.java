fccd3a81ac7350c69289b7d1a74cb316132bdd0d
valensssia <99199656+valensssia@users.noreply.github.com>
2022-02-23 19:29:26 +0200

    Add files via upload

---
+++ podil_yabluk_1.java
@@ -0,0 +1,14 @@
+package input_output;
+
+import java.util.Scanner;
+
+public class podil_yabluk_1 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		System.out.println(k / n);
+    }
+}
+
+
