5250c634f246cde9967e81e54de7dab0ad3fa9ed
valensssia <99199656+valensssia@users.noreply.github.com>
2022-02-23 19:34:02 +0200

    Add files via upload

---
+++ ostsnnya_cyfra.java
@@ -0,0 +1,14 @@
+package input_output;
+
+import java.util.Scanner;
+
+public class ostsnnya_cyfra {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+        System.out.println(n % 10);
+
+    }
+}
+
+
